36f3bbfe851c0436c6a7bc8df276b591a46aab4a
sheytoonch <83016208+sheytoonch@users.noreply.github.com>
2025-06-15 10:29:06 +0200

    new dates

--- userData.js
+++ userData.js
@@ -2,116 +2,40 @@
 
 const busyDates = [
 
-    [2024, 6, 22, "tajne rzeczy"],
-    [2024, 6, 23, "tajne rzeczy"],
-    [2024, 6, 29, "tajne rzeczy"],
-    [2024, 6, 30, "tajne rzeczy"],
-    [2024, 7, 6, "tajne rzeczy"],
-    [2024, 7, 7, "tajne rzeczy"],
-    [2024, 7, 20, "tajne rzeczy"],
-    [2024, 7, 21, "tajne rzeczy"],
-    [2024, 8, 3, "tajne rzeczy"],
-    [2024, 8, 4, "tajne rzeczy"],
-    [2024, 8, 17, "tajne rzeczy"],
-    [2024, 8, 18, "tajne rzeczy"],
-    [2024, 8, 31, "tajne rzeczy"],
-    [2024, 9, 1, "tajne rzeczy"],
-    [2024, 9, 14, "tajne rzeczy"],
-    [2024, 9, 15, "tajne rzeczy"],
-    [2024, 9, 28, "tajne rzeczy"],
-    [2024, 9, 29, "tajne rzeczy"],
-    [2024, 10, 12, "tajne rzeczy"],
-    [2024, 10, 13, "tajne rzeczy"],
-    [2024, 10, 26, "tajne rzeczy"],
-    [2024, 10, 27, "tajne rzeczy"],
+    [2025, 6, 25, "morze"],
+    [2025, 6, 26, "morze"],
+    [2025, 6, 27, "morze"],
 
-    [2024, 6, 26, "lekcje"],
-    [2024, 7, 3, "lekcje"],
-    [2024, 7, 10, "lekcje"],
-    [2024, 7, 17, "lekcje"],
-    [2024, 7, 24, "lekcje"],
-    [2024, 7, 31, "lekcje"],
+    [2025, 7, 4, "opener"],
 
-    [2024, 8, 23, "malarstwo"],
-    [2024, 8, 24, "malarstwo"],
-    [2024, 8, 25, "malarstwo"],
-
-    [2024, 8, 21, "dr"],
-    [2024, 9, 30, "dr"],
-    [2025, 1, 22, "dr"],
-
-    [2024, 10, 19, "wycieczka"],
-    [2024, 10, 20, "wycieczka"],
-    [2024, 10, 21, "wycieczka"],
-    [2024, 10, 22, "wycieczka"],
-    [2024, 10, 23, "wycieczka"],
-    [2024, 10, 24, "wycieczka"],
-    [2024, 10, 25, "wycieczka"],
-    [2024, 10, 26, "wycieczka"],
-    [2024, 10, 27, "wycieczka"],
+    [2025, 7, 31, "wood"],
+    [2025, 8, 1, "wood"],
+    [2025, 8, 2, "wood"],
+    [2025, 8, 3, "wood"]
 
 
 ];
 
 const notSureDates = [
 
-    [2024, 11, 1, "wakacje"],
-    [2024, 11, 2, "wakacje"],
-    [2024, 11, 3, "wakacje"],
-    [2024, 11, 4, "wakacje"],
-    [2024, 11, 5, "wakacje"],
-    [2024, 11, 6, "wakacje"],
-    [2024, 11, 7, "wakacje"],
-    [2024, 11, 8, "wakacje"],
-    [2024, 11, 9, "wakacje"],
-    [2024, 11, 10, "wakacje"],
-    [2024, 11, 11, "wakacje"],
-    [2024, 11, 12, "wakacje"],
-    [2024, 11, 13, "wakacje"],
-    [2024, 11, 14, "wakacje"],
-    [2024, 11, 15, "wakacje"],
-    [2024, 11, 16, "wakacje"],
-    [2024, 11, 17, "wakacje"],
-    [2024, 11, 18, "wakacje"],
-    [2024, 11, 19, "wakacje"],
-    [2024, 11, 20, "wakacje"],
-    [2024, 11, 21, "wakacje"],
-    [2024, 11, 22, "wakacje"],
-    [2024, 11, 23, "wakacje"],
-    [2024, 11, 24, "wakacje"],
-    [2024, 11, 25, "wakacje"],
-    [2024, 11, 26, "wakacje"],
-    [2024, 11, 27, "wakacje"],
-    [2024, 11, 28, "wakacje"],
-    [2024, 11, 29, "wakacje"],
-    [2024, 11, 30, "wakacje"],
-    [2024, 12, 1, "wakacje"],
-    [2024, 12, 2, "wakacje"],
-    [2024, 12, 3, "wakacje"],
-    [2024, 12, 4, "wakacje"],
-    [2024, 12, 5, "wakacje"],
-    [2024, 12, 6, "wakacje"],
-    [2024, 12, 7, "wakacje"],
-    [2024, 12, 8, "wakacje"],
-    [2024, 12, 9, "wakacje"],
-    [2024, 12, 10, "wakacje"],
-    [2024, 12, 11, "wakacje"],
-    [2024, 12, 12, "wakacje"],
-    [2024, 12, 13, "wakacje"],
-    [2024, 12, 14, "wakacje"],
-    [2024, 12, 15, "wakacje"],
-    [2024, 12, 16, "wakacje"],
-
-    [2024, 8, 16, "picie"],
-
+    [2025, 7, 29, "tajne rzeczy"],
 
 ]
 
 const doNotDisturbDates = [
-    [2024, 4, 5, "busy"],
-    [2024, 4, 8, "busy"],
-    [2024, 4, 9, "busy"],
-    [2024, 4, 18, "busy"]
+
+    [2025, 6, 28, "morze"],
+
+    [2025, 7, 5, "opener"],
+
+    [2025, 7, 30, "wood"],
+
+    [2025, 9, 10, "oczy"]
+
 ]
 
-const freeDates = [];
+const freeDates = [
+
+
+
+];
