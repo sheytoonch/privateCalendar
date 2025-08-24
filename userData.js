19c6c6136a601cf545203200c23911b987244dc4
sheytoonch <83016208+sheytoonch@users.noreply.github.com>
2025-08-24 19:54:14 +0200

    dfs

--- userData.js
+++ userData.js
@@ -8,6 +8,8 @@ const busyDates = [
     [2025, 8, 7, "lekarz"],
     [2025, 8, 14, "lekarz"],
     [2025, 8, 21, "lekarz"],
+    [2025, 9, 24, "tajne rzeczy"],
+    [2025, 9, 28, "tajne rzeczy"],
 
     [2025, 10, 22, "opera"],
     [2025, 12, 30, "spłata"]
