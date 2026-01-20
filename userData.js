29128c7e0c24b496bf80bde18e302e3e3d7bec0d
sheytoonch <83016208+sheytoonch@users.noreply.github.com>
2026-01-20 10:00:34 +0100

    asa

--- userData.js
+++ userData.js
@@ -1,40 +1,57 @@
 // Example format: [year, month, day, description]
 
 const busyDates = [
+    [2026, 7, 20, "zakopane"],
+    [2026, 7, 21, "zakopane"],
+    [2026, 7, 22, "zakopane"],
+    [2026, 7, 23, "zakopane"],
+    [2026, 7, 24, "zakopane"],
+    [2026, 7, 25, "zakopane"],
+    [2026, 7, 26, "zakopane"],
+    [2026, 7, 27, "zakopane"],
 
-    [2025, 8, 24, "tajne rzeczy"],
-
-
-    [2025, 8, 7, "lekarz"],
-    [2025, 8, 14, "lekarz"],
-    [2025, 8, 21, "lekarz"],
-    [2025, 9, 24, "tajne rzeczy"],
-    [2025, 9, 28, "tajne rzeczy"],
-
-    [2025, 10, 11, "urodziny"],
-    [2025, 10, 18, "urodziny"],
-    [2025, 10, 22, "opera"],
-    [2025, 10, 25, "poznan"],
-    [2025, 12, 30, "spłata"]
-
+    [2026, 3, 12, "tajne rzeczy"],
+    [2026, 3, 17, "tajne rzeczy"],
+    [2026, 3, 19, "tajne rzeczy"],
+    [2026, 3, 24, "tajne rzeczy"],
+    [2026, 3, 26, "tajne rzeczy"],
+    [2026, 3, 31, "tajne rzeczy"],
+    [2026, 4, 2, "tajne rzeczy"],
+    [2026, 4, 7, "tajne rzeczy"],
+    [2026, 4, 9, "tajne rzeczy"],
+    [2026, 4, 14, "tajne rzeczy"],
+    [2026, 4, 16, "tajne rzeczy"],
+    [2026, 4, 21, "tajne rzeczy"],
+    [2026, 4, 23, "tajne rzeczy"],
+    [2026, 4, 28, "tajne rzeczy"],
+    [2026, 4, 30, "tajne rzeczy"],
+    [2026, 5, 5, "tajne rzeczy"],
+    [2026, 5, 7, "tajne rzeczy"],
+    [2026, 5, 12, "tajne rzeczy"],
+    [2026, 5, 14, "tajne rzeczy"],
+    [2026, 5, 19, "tajne rzeczy"],
+    [2026, 5, 21, "tajne rzeczy"],
+    [2026, 5, 26, "tajne rzeczy"],
+    [2026, 5, 28, "tajne rzeczy"],
+    [2026, 6, 2, "tajne rzeczy"],
+    [2026, 6, 9, "tajne rzeczy"],
+    [2026, 6, 11, "tajne rzeczy"],
+    [2026, 6, 16, "tajne rzeczy"],
+    [2026, 6, 18, "tajne rzeczy"],
+    [2026, 6, 23, "tajne rzeczy"],
+    [2026, 6, 25, "tajne rzeczy"],
+    [2026, 6, 30, "tajne rzeczy"]
 
 ];
 
 const notSureDates = [
-    [2025, 10, 20, "wiki"],
-    [2025, 10, 21, "kris"]
+
 ]
 
 const doNotDisturbDates = [
 
-    [2026, 7, 21, "ben"],
-
-    [2025, 11, 12, "szum"],
-    [2025, 11, 13, "tom"]
 ]
 
 const freeDates = [
-
-
 
 ];
